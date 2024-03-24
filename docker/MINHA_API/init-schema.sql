cbc9099b3cbbb3400b45ca5aed1541b84aed7c7e
Heitor Fernando Barbosa Ramos <57812397+heitorbarbosaramos@users.noreply.github.com>
2024-03-24 19:41:18 -0300

    criando schema para o back end

--- docker/MINHA_API/init-schema.sql
+++ docker/MINHA_API/init-schema.sql
@@ -3,9 +3,17 @@ BEGIN
     IF NOT EXISTS(
         SELECT schema_name
         FROM information_schema.schemata
-        WHERE schema_name = 'minhaApi_schema'
+        WHERE schema_name = 'minhaApi_keycloack'
     ) THEN
-        EXECUTE 'CREATE SCHEMA minhaApi_schema';
+        EXECUTE 'CREATE SCHEMA minhaApi_keycloack';
+    END IF;
+
+     IF NOT EXISTS(
+        SELECT schema_name
+        FROM information_schema.schemata
+        WHERE schema_name = 'minhaApi_service'
+    ) THEN
+        EXECUTE 'CREATE SCHEMA minhaApi_service';
     END IF;
 END
 $$;
