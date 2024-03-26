e1bf1e45fe017704351d7338de36c2181fd49126
Heitor Fernando Barbosa Ramos <57812397+heitorbarbosaramos@users.noreply.github.com>
2024-03-25 22:04:15 -0300

    configurando variavel de ambiente realm

--- service/src/main/java/com/heitor/minhaApi/security/feignClient/KeycloakClient.java
+++ service/src/main/java/com/heitor/minhaApi/security/feignClient/KeycloakClient.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "keycloakClient", url = "http://localhost:8090/auth")
 public interface KeycloakClient {
 
-    @PostMapping(value = "/realms/REALM_MINHAAPI/protocol/openid-connect/token", consumes = "application/x-www-form-urlencoded")
+    @PostMapping(value = "/realms/${keycloak.client.realm}/protocol/openid-connect/token", consumes = "application/x-www-form-urlencoded")
     TokenResponse getToken(@RequestHeader("Content-Type") String contentType, @RequestBody TokenRequest request);
 
-    @PostMapping(value = "/realms/REALM_MINHAAPI/protocol/openid-connect/token", consumes = "application/x-www-form-urlencoded")
+    @PostMapping(value = "/realms/${keycloak.client.realm}/protocol/openid-connect/token", consumes = "application/x-www-form-urlencoded")
     TokenResponse refreshToken(@RequestHeader("Content-Type") String contentType, @RequestBody TokenRefreshRequest request);
 
-    @GetMapping(value = "/realms/REALM_MINHAAPI/protocol/openid-connect/userinfo")
+    @GetMapping(value = "/realms/${keycloak.client.realm}/protocol/openid-connect/userinfo")
     UserInfoResponse getUserInfo(@RequestHeader("Authorization") String Authorization);
 
 }
